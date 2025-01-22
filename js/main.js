6192f980af1f587e7746d3c99f6f1740b72b6c5f
KANGSEUNGHOON <xgde0324@gmail.com>
2025-01-22 18:10:37 +0900

    스와이퍼 다 적용

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
 window.onload = function () {
-  const itemSwiper = new Swiper(".main4Swiper", {
-    // loop: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
+  /* 스와이퍼 1 */
+  const mainSwiper = new Swiper(".main1Swiper", {
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+  });
+  /* 스와이퍼 3 */
+  const menuSwiper = new Swiper(".main3Swiper", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+
     breakpoints: {
-     
       768: {
-        slidesPerView: 2,
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1050: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
+  /* 스와이퍼 4 */
+  const itemSwiper = new Swiper(".main4Swiper", {
+    loop: true,
+    slidesPerView: 1.5,
+    spaceBetween: 20,
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
       },
       1050: {
         slidesPerView: 3,
+        spaceBetween: 20,
       },
     },
   });
