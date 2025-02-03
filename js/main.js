f71b08391009aecb8780486a744a3067c453cbb8
KANGSEUNGHOON <xgde0324@gmail.com>
2025-02-03 18:12:43 +0900

    메뉴창 띄우기 진행 중

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ window.onload = function () {
   });
   /* 스와이퍼 3 */
   const menuSwiper = new Swiper(".main3Swiper", {
-    
     slidesPerView: 1.7,
     spaceBetween: 20,
 
@@ -40,7 +39,6 @@ window.onload = function () {
   });
   /* 스와이퍼 4 */
   const itemSwiper = new Swiper(".main4Swiper", {
-    
     slidesPerView: 1.7,
     spaceBetween: 20,
 
@@ -54,5 +52,56 @@ window.onload = function () {
         spaceBetween: 20,
       },
     },
+  });
+  // 햄버거바 메뉴창 열기
+  const modal = document.querySelector(".hamburger-menu-wrap");
+  const modalOpen = document.querySelector(".hamburger");
+  modalOpen.addEventListener("click", function () {
+    modal.style.display = "block";
+    modalOpen.style.display = "none";
+  });
+  // 햄버거바 메뉴창 닫기
+
+  const modalClose = document.querySelector(".menu-close");
+  modalClose.addEventListener("click", function () {
+    modal.style.display = "none";
+    modalOpen.style.display = "block";
+  });
+  //상단 메뉴바 호버
+  //브랜드
+  const brand = document.querySelector(".brandhv");
+  const brandList = document.querySelector(".brandhv-list");
+  brand.addEventListener("mouseenter", function () {
+    brandList.style.display = "flex";
+  });
+  brand.addEventListener("mouseleave", function () {
+    brandList.style.display = "none";
+  });
+  //메뉴
+  const menu = document.querySelector(".menuhv");
+  const menuList = document.querySelector(".menuhv-list");
+  menu.addEventListener("mouseenter", function () {
+    menuList.style.display = "flex";
+  });
+  menu.addEventListener("mouseleave", function () {
+    menuList.style.display = "none";
+  });
+  //이벤트
+  const event = document.querySelector(".eventhv");
+  const eventList = document.querySelector(".eventhv-list");
+  event.addEventListener("mouseenter", function () {
+    eventList.style.display = "flex";
+  });
+  event.addEventListener("mouseleave", function () {
+    eventList.style.display = "none";
+  });
+  //오더
+  const order = document.querySelector(".orderhv");
+  const orderList = document.querySelector(".orderhv-list");
+  order.addEventListener("mouseenter", function () {
+    orderList.style.display = "flex";
+  });
+  order.addEventListener("mouseleave", function () {
+    orderList.style.display = "none";
   });
 };
